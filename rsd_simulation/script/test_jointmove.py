ffde0eee42b816dae5e7ee7afebbdbe52ffdb366
Jinhyeok1489 <songjinhuk@dgist.ac.kr>
2021-11-08 21:31:48 +0900

    PRRP robot works in rviz but not work in Gazebo simulator
    PRRP robot works in rviz but not work in Gazebo simulator..
    
    It works in rviz simulation with joint_publisher_gui

---
+++ rsd_simulation/script/test_jointmove.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+# license removed for brevity
+import rospy
+import math
+import threading
+
+# import getch
+import numpy as np
+import sys, select, termios, tty
+from std_msgs.msg import Float64
+from sensor_msgs.msg import Imu
+from rosgraph_msgs.msg import Clock
+from snake_control.msg import GaitParameter
+from snake_control.msg import ParamtoSine
+from snake_control.msg import DstTime
+from gazebo_msgs.msg import LinkStates
+from geometry_msgs.msg import Pose
+
+if __name__ == '__main__':
+    rospy.init_node('test_move', anonymous=True)
+
+    pub_j1 = rospy.Publisher('/prrp_control/joint_1_position_controller/command', Float64, queue_size=10)
+    pub_j2 = rospy.Publisher('/prrp_control/joint_2_position_controller/command', Float64, queue_size=10)
+    pub_j3 = rospy.Publisher('/prrp_control/joint_3_position_controller/command', Float64, queue_size=10)
+    pub_j4 = rospy.Publisher('/prrp_control/joint_4_position_controller/command', Float64, queue_size=10)
+
+    rate = rospy.Rate(20)
+
+    while not rospy.is_shutdown():
+        pub_j1.publish(0.1)
+        pub_j2.publish(math.pi)
+        pub_j3.publish(math.pi/4)
+        pub_j4.publish(-0.1)
